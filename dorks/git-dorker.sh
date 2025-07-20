6d0e1cc5f9ac4e86a638276b3fd8193c8749cba4
r4gh4v <96167320+ir4gh4v@users.noreply.github.com>
2025-07-20 14:59:41 +0530

    Create git-dorker.sh

---
+++ dorks/git-dorker.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+host=$1
+
+github_recon() {
+for domain in $(cat $host);
+do
+mkdir -p github-results
+echo -e "\n${CYAN}
+\t+-+-+-+-+-+-+
+\t|R|4|G|H|4|V|
+\t+-+-+-+-+-+-+
+${RESET}\n"
+python3 GitDorker.py -tf token.txt -q $domain -d /root/tools/wordlists/keywords.txt -o github-results/$domain/results.txt
+done
+}
+github_recon
